b6a3c56925045a523b0cc46b6a90236b729fadb7
Win <erwincurato@gmail.com>
2026-09-08 22:49:24 +0800

    test: ignore local demo projects

--- vitest.config.js
+++ vitest.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    exclude: ['**/node_modules/**', '**/.git/**', '**/.worktrees/**'],
+    exclude: ['**/node_modules/**', '**/.git/**', '**/.worktrees/**', '**/.demo/**'],
   },
 })
